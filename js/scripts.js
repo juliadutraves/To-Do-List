bc3f86c206035faf88f7ad5df0c243039fb67f62
Julia Alves <julia.dutra@alunoescolassociais.org.br>
2024-06-13 09:11:11 -0300

    Adiciona Local Storage.

--- js/scripts.js
+++ js/scripts.js
@@ -141,4 +141,57 @@ filterBtn.addEventListener("change", (e) => {
  
   filterTodos(filterValue);
 });
+
+// Local Storage
+const getTodosLocalStorage = () => {
+  const todos = JSON.parse(localStorage.getItem("todos")) || [];
+ 
+  return todos;
+};
+ 
+const loadTodos = () => {
+  const todos = getTodosLocalStorage();
+ 
+  todos.forEach((todo) => {
+    saveTodo(todo.text, todo.done, 0);
+  });
+};
+ 
+const saveTodoLocalStorage = (todo) => {
+  const todos = getTodosLocalStorage();
+ 
+  todos.push(todo);
+ 
+  localStorage.setItem("todos", JSON.stringify(todos));
+};
+ 
+const removeTodoLocalStorage = (todoText) => {
+  const todos = getTodosLocalStorage();
+ 
+  const filteredTodos = todos.filter((todo) => todo.text != todoText);
+ 
+  localStorage.setItem("todos", JSON.stringify(filteredTodos));
+};
+ 
+const updateTodoStatusLocalStorage = (todoText) => {
+  const todos = getTodosLocalStorage();
+ 
+  todos.map((todo) =>
+    todo.text === todoText ? (todo.done = !todo.done) : null
+  );
+ 
+  localStorage.setItem("todos", JSON.stringify(todos));
+};
+ 
+const updateTodoLocalStorage = (todoOldText, todoNewText) => {
+  const todos = getTodosLocalStorage();
+ 
+  todos.map((todo) =>
+    todo.text === todoOldText ? (todo.text = todoNewText) : null
+  );
+ 
+  localStorage.setItem("todos", JSON.stringify(todos));
+};
+ 
+loadTodos();
  
