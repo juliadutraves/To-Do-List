ea939c4e75e9e91d4c01c3747df0a911d5326e63
Julia Alves <julia.dutra@alunoescolassociais.org.br>
2024-06-13 09:23:57 -0300

    Arruma javascrit, pis deu errdo as chaves.

--- js/scripts.js
+++ js/scripts.js
@@ -8,63 +8,117 @@ const cancelEditBtn = document.querySelector("#cancel-edit-btn");
 const searchInput = document.querySelector("#search-input");
 const eraseBtn = document.querySelector("#erase-button");
 const filterBtn = document.querySelector("#filter-select");
-
+ 
 let oldInputValue;
-
+ 
 // Funções
 const saveTodo = (text, done = 0, save = 1) => {
   const todo = document.createElement("div");
   todo.classList.add("todo");
-
+ 
   const todoTitle = document.createElement("h3");
   todoTitle.innerText = text;
   todo.appendChild(todoTitle);
-
+ 
   const doneBtn = document.createElement("button");
   doneBtn.classList.add("finish-todo");
   doneBtn.innerHTML = '<i class="fa-solid fa-check"></i>';
   todo.appendChild(doneBtn);
-
+ 
   const editBtn = document.createElement("button");
   editBtn.classList.add("edit-todo");
   editBtn.innerHTML = '<i class="fa-solid fa-pen"></i>';
   todo.appendChild(editBtn);
-
+ 
   const deleteBtn = document.createElement("button");
   deleteBtn.classList.add("remove-todo");
   deleteBtn.innerHTML = '<i class="fa-solid fa-xmark"></i>';
   todo.appendChild(deleteBtn);
-
+ 
   // Utilizando dados da localStorage
   if (done) {
     todo.classList.add("done");
   }
-
+ 
   if (save) {
     saveTodoLocalStorage({ text, done: 0 });
   }
-
+ 
   todoList.appendChild(todo);
-
+ 
   todoInput.value = "";
 };
-
+ 
 const toggleForms = () => {
   editForm.classList.toggle("hide");
   todoForm.classList.toggle("hide");
   todoList.classList.toggle("hide");
 };
-
+ 
 const updateTodo = (text) => {
   const todos = document.querySelectorAll(".todo");
-
+ 
   todos.forEach((todo) => {
     let todoTitle = todo.querySelector("h3");
-
+ 
     if (todoTitle.innerText === oldInputValue) {
       todoTitle.innerText = text;
-
-      // Eventos
+ 
+      // Utilizando dados da localStorage
+      updateTodoLocalStorage(oldInputValue, text);
+    }
+  });
+};
+ 
+const getSearchedTodos = (search) => {
+  const todos = document.querySelectorAll(".todo");
+ 
+  todos.forEach((todo) => {
+    const todoTitle = todo.querySelector("h3").innerText.toLowerCase();
+ 
+    todo.style.display = "flex";
+ 
+    console.log(todoTitle);
+ 
+    if (!todoTitle.includes(search)) {
+      todo.style.display = "none";
+    }
+  });
+};
+ 
+const filterTodos = (filterValue) => {
+  const todos = document.querySelectorAll(".todo");
+ 
+  switch (filterValue) {
+    case "all":
+      todos.forEach((todo) => (todo.style.display = "flex"));
+ 
+      break;
+ 
+    case "done":
+      todos.forEach((todo) =>
+        todo.classList.contains("done")
+          ? (todo.style.display = "flex")
+          : (todo.style.display = "none")
+      );
+ 
+      break;
+ 
+    case "todo":
+      todos.forEach((todo) =>
+        !todo.classList.contains("done")
+          ? (todo.style.display = "flex")
+          : (todo.style.display = "none")
+      );
+ 
+      break;
+ 
+    default:
+      break;
+  }
+};
+ 
+// Eventos
 todoForm.addEventListener("submit", (e) => {
   e.preventDefault();
  
@@ -91,8 +145,8 @@ document.addEventListener("click", (e) => {
   }
  
   if (targetEl.classList.contains("remove-todo")) {
-    parentEl.remove()
-
+    parentEl.remove();
+ 
     // Utilizando dados da localStorage
     removeTodoLocalStorage(todoTitle);
   }
@@ -141,7 +195,7 @@ filterBtn.addEventListener("change", (e) => {
  
   filterTodos(filterValue);
 });
-
+ 
 // Local Storage
 const getTodosLocalStorage = () => {
   const todos = JSON.parse(localStorage.getItem("todos")) || [];
@@ -194,4 +248,3 @@ const updateTodoLocalStorage = (todoOldText, todoNewText) => {
 };
  
 loadTodos();
- 
